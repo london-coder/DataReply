b414df1ee54397635042eba3894c6bb29c55edbe
Alan Lewis <alan.lewis42@gmail.com>
2018-09-21 14:27:22 +0100

    Diamond drawing function testing ok.

---
+++ src/main/scala/example/Diamonds.scala
@@ -0,0 +1,21 @@
+package example
+
+object Diamonds extends Greeting with App {
+  def greeting = "hello"	// because the template creates a test for this!
+  1 to 7 foreach (diamond(_))
+}
+
+trait Greeting {
+  lazy val howmany: String = "how many? "
+
+  def diamond(size: Int): Unit = {
+  	recursiveDiamond(1, size - (size/2+1))
+  }
+
+  def recursiveDiamond(count: Int, spaces: Int): Unit = {
+  	println( " "*Math.abs( spaces ) + "o"*count )
+  	val nspaces = spaces - 1
+  	val ncount = count + (if(nspaces >= 0) 2 else -2)
+	if(count > 0) recursiveDiamond(ncount, nspaces)
+  }
+}
