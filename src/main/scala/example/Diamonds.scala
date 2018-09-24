28812a69e1af5cb7e2d00ee12f65010a603ee66e
Alan Lewis <alan.lewis42@gmail.com>
2018-09-24 15:58:23 +0100

    Fix code format.

--- src/main/scala/example/Diamonds.scala
+++ src/main/scala/example/Diamonds.scala
@@ -8,14 +8,14 @@ object Diamonds extends Greeting with App {
 trait Greeting {
 
   def diamond(size: Int): Unit = {
-  	recursiveDiamond(1, size - (size/2+1))
+    recursiveDiamond(1, size - (size/2+1))
   }
   import scala.annotation.tailrec
 
   @tailrec final def recursiveDiamond(count: Int, spaces: Int): Unit = {
-  	println( " "*Math.abs( spaces ) + "o"*count )
-  	val nspaces = spaces - 1
-  	val ncount = count + (if(nspaces >= 0) 2 else -2)
-	  if(count > 0) recursiveDiamond(ncount, nspaces)
+    println( " "*Math.abs( spaces ) + "o"*count )
+    val nspaces = spaces - 1
+    val ncount = count + (if(nspaces >= 0) 2 else -2)
+    if(count > 0) recursiveDiamond(ncount, nspaces)
   }
 }
